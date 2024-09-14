4cd21323a0eb5135f2e59d61909345bc0050428f
Kanishk Agrawal <kanishk71@gmail.com>
2024-09-14 07:49:34 +0530

    create ticket dynamically

--- Lec-18 - Kanban/script.js
+++ Lec-18 - Kanban/script.js
@@ -2,6 +2,8 @@
 
 const addBtn = document.querySelector('.add-btn');
 const modalCont = document.querySelector('.modal-cont');
+const textAreaCont = document.querySelector('.textArea-cont');
+
 
 let addTaskFlag = false; // on page load initally, popup should not be visible.
 
@@ -22,3 +24,35 @@ addBtn.addEventListener('click', function () {
     }
 
 })
+
+// create a ticket dynamically.
+
+function createTicket() {
+    //create a new ticket HTML (container element)
+    const ticketCont = document.createElement('div');
+    ticketCont.classList.add('ticket-cont');
+    // OR
+    // ticketCont.setAttribute('class', 'ticket-cont')
+    ticketCont.innerHTML = `
+        <div class="ticket-color"></div>
+        <div class="ticket-id">123</div>
+        <div class="ticket-area">Study today.</div>
+        <div class="ticket-lock">
+            <i class="fa-solid fa-lock"></i>
+            <!-- <i class="fa-solid fa-lock-open"></i> -->
+        </div>
+    `;
+    const mainCont = document.querySelector('.main-cont');
+    mainCont.appendChild(ticketCont);
+}
+
+modalCont.addEventListener('keydown', function (ev) {
+    if (ev.key === 'Shift') {
+        createTicket()
+        modalCont.style.display = 'none'; // hide the modal.
+        textAreaCont.value = ''; // clear contents on close.
+    };
+
+
+})
+
