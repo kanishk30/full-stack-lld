0fc9df4f8520bb0679ade538d81abb3ef2e873b0
Kanishk Agrawal <kanishk71@gmail.com>
2024-09-26 07:50:15 +0530

    capturing

--- Lec-22 - Event propagation & MC-1/scripts.js
+++ Lec-22 - Event propagation & MC-1/scripts.js
@@ -2,18 +2,32 @@ const grandparent = document.querySelector('#grandparent');
 const parent = document.querySelector('#parent');
 const child = document.querySelector('#child');
 
-grandparent.addEventListener('click', function () {
-    console.log('grandparent clicked')
-})
+// grandparent.addEventListener('click', function () {
+//     console.log('grandparent clicked')
+// })
 
-parent.addEventListener('click', function () {
-    console.log('parent clicked')
-})
-child.addEventListener('click', function () {
-    console.log('child clicked')
-})
+// parent.addEventListener('click', function () {
+//     console.log('parent clicked')
+// })
+// child.addEventListener('click', function () {
+//     console.log('child clicked')
+// })
 
 // OUTPUT for event bubbling when child is clicked.
 // child clicked
 // scripts.js:10 parent clicked
 // scripts.js:6 grandparent clicked
+
+
+// EVENT CAPTURING STARTS...
+
+grandparent.addEventListener('click', function () {
+    console.log('grandparent clicked (capturing)')
+}, true)
+
+parent.addEventListener('click', function () {
+    console.log('parent clicked (capturing)')
+}, true)
+child.addEventListener('click', function () {
+    console.log('child clicked (capturing)')
+}, true)
