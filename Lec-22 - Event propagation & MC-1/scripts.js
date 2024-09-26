f61049edd5dafed5d3407c79d677ce00bbfcc6bb
Kanishk Agrawal <kanishk71@gmail.com>
2024-09-26 07:32:01 +0530

    event  bubbling

---
+++ Lec-22 - Event propagation & MC-1/scripts.js
@@ -0,0 +1,19 @@
+const grandparent = document.querySelector('#grandparent');
+const parent = document.querySelector('#parent');
+const child = document.querySelector('#child');
+
+grandparent.addEventListener('click', function () {
+    console.log('grandparent clicked')
+})
+
+parent.addEventListener('click', function () {
+    console.log('parent clicked')
+})
+child.addEventListener('click', function () {
+    console.log('child clicked')
+})
+
+// OUTPUT for event bubbling when child is clicked.
+// child clicked
+// scripts.js:10 parent clicked
+// scripts.js:6 grandparent clicked
