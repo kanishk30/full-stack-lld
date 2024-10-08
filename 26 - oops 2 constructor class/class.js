5451b76b653e999a152cb9f501e68e8bfc44a1b2
Kanishk Agrawal <kanishk71@gmail.com>
2024-10-08 08:58:34 +0530

    using static

--- 26 - oops 2 constructor class/class.js
+++ 26 - oops 2 constructor class/class.js
@@ -28,23 +28,51 @@ const className = new MyClass()
 
 
 class Pizza {
+    static totalPizzasMade = 0;
     constructor(toppings, size, crustType) {
         this.toppings = toppings;
         this.size = size;
         this.crustType = crustType;
+        Pizza.totalPizzasMade++; // increment the count each time a new pizza is made.
     }
 
     describe() {
         console.log(`I had ${this.size} ${this.crustType} pizza with ${this.toppings.join(', ')}.`)
+    }
+
+    static calculatePizzasMade() {
+        console.log('Total pizzas: ', Pizza.totalPizzasMade)
     }
 }
 
 const ravisOrder = new Pizza(['Cheese', 'Pepperoni'], 'Small', 'thin');
 const vinaysOrder = new Pizza(['Cheese', 'Veggies'], 'Large', 'thick');
 
-ravisOrder.describe()
-vinaysOrder.describe()
+// ravisOrder.describe()
+// vinaysOrder.describe()
 
+
+class StuffedCrustPizza extends Pizza {
+    constructor(toppings, size, crustType, stuffingType) {
+        super(toppings, size, crustType); // Call the parent constrcutor with super.
+        this.stuffingType = stuffingType;
+    }
+
+    describeStuffing() {
+        console.log(`This pizza has ${this.stuffingType} stuffing in it.`)
+    }
+
+    describe() {
+        super.describe(); // Call the describe method from the parent's class.
+    }
+}
+
+const specialOrder = new StuffedCrustPizza(['Cheese', 'Mushrooms'], 'medium', 'wheat', 'corn and cheese')
+
+specialOrder.describe();
+specialOrder.describeStuffing();
+
+Pizza.calculatePizzasMade() // 3
 
 
 
