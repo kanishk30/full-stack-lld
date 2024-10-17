8c5bf9694564bd3fd7bab6086dbe1a2be1cccfe1
Kanishk Agrawal <kanishk71@gmail.com>
2024-10-17 07:53:11 +0530

    Polyfill of map

---
+++ 29 - polyfills of HOF/map.js
@@ -0,0 +1,52 @@
+const arr = [1, 2, 3];
+const result1 = arr.map((item) => item * item);
+// map returns a new array && do not modify original array.
+// console.log(result1);
+
+const res2 = arr.map(function (el, index, origArr) {
+    // console.log(index, origArr)
+    // console.log(this)
+    return { [index]: el }
+})
+
+// (method) Array<number>.map<number>(callbackfn: (value: number, index: number, array: number[]) => number, thisArg?: any): number[]
+// console.log(res2);
+
+// if(!Array.prototype.map) {
+//     Array.prototype.map = function()
+// }
+
+Array.prototype.myMap = function (callbackfn, thisArg) {
+    if (typeof callbackfn !== 'function') {
+        throw new TypeError(callbackfn + ' should be a function.')
+    }
+    if (!Array.isArray(this)) throw new TypeError(this + ' should be an array')
+    // initialising a new result array
+    const arr = new Array(this.length); // make a new array of length of "this" original array.
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i in this) {
+            var context = thisArg ? thisArg : this;
+            const mappedValue = callbackfn.call(context, this[i], i, this);
+            arr[i] = mappedValue
+        }
+    }
+    return arr;
+}
+
+
+const result3 = [1, 2, , 3].myMap((item) => item * item);
+const result4 = [1, 2, , 3].map((item) => item * item);
+// console.log(result3);
+// console.log(result4);
+
+const myArg = { multiplier: 3 };
+
+const ans = [1, 2, 3].myMap(function (el) {
+    return el * this.multiplier
+}, myArg)
+
+console.log(ans)
+
+
+
