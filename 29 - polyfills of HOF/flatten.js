dc210e806682f3bfc7f7d189d44507e5578f577e
Kanishk Agrawal <kanishk71@gmail.com>
2024-10-17 09:18:18 +0530

    Flatten array

---
+++ 29 - polyfills of HOF/flatten.js
@@ -0,0 +1,23 @@
+const nestedArr = [1, [2, 3], [4, [5, [6]]], {}, '', null, undefined, true, { a: 1 }]
+const nestedArr1 = [1, [2, 3], [4, [5, [6]]]]
+// expected output = [1,2,3,4,5,6]
+// return arr.flat(Infinity)
+
+function flattenFunc(arr) {
+    return arr.reduce((flatArray, currValue) => {
+        if (Array.isArray(currValue)) {
+            // faltten it..
+            // const val = flattenFunc(currValue);
+            // console.log('val', val)
+            // flatArray.push(...val)
+            // SAME AS ABOVE
+            flatArray.push(...flattenFunc(currValue))
+        } else {
+            // simply push to my result. (flatArray)
+            flatArray.push(currValue)
+        }
+        return flatArray;
+    }, [])
+}
+
+console.log(flattenFunc(nestedArr1)); // [1,2,3,4,5,6]
