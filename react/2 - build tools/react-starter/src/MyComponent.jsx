14c693a2c5d45aea7d4b9ba192a7f69e536d30e7
Kanishk Agrawal <kanishk71@gmail.com>
2024-11-07 08:31:02 +0530

    Using props

---
+++ react/2 - build tools/react-starter/src/MyComponent.jsx
@@ -0,0 +1,16 @@
+
+const MyComponent = (props) => {
+    // console.log(props)
+    return <h1>Hello, I am {props.message} {props.name}</h1>
+}
+
+// function MyComponent() {
+//     return <h1>Hello, I am Component 1</h1>
+// }
+
+export const a = 1;
+export const b = 2;
+export const c = 3;
+
+
+export default MyComponent;
