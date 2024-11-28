2da4f50640d1d62298456ca9c1788ee1eca9b0a9
Kanishk Agrawal <kanishk71@gmail.com>
2024-11-28 08:32:46 +0530

    Unit & snapshot test

--- react/testing/src/components/test/Counter.test.js
+++ react/testing/src/components/test/Counter.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Counter from '../Counter'
 /**
@@ -9,25 +9,41 @@ import Counter from '../Counter'
  *      - on decrement btn click, new count shud be -1.
  */
 
-test('initial state check', () => {
-    // rendering in VDOM
-    render(<Counter />);
+describe('Counter component testcases', () => {
+    test('initial state check', () => {
+        // rendering in VDOM
+        render(<Counter></Counter>);
 
-    // selection..
-    const countText = screen.getByText('Count is 0');
-    const decrBtn = screen.getByText('Decrement');
-    const incrBtn = screen.getByText('Increment');
+        // selection..
+        const countText = screen.getByText('Count is 0');
+        const decrBtn = screen.getByText('Decrement');
+        const incrBtn = screen.getByText('Increment');
 
-    // we are verifying...
-    expect(countText).toBeInTheDocument()
-    expect(decrBtn).toBeInTheDocument()
-    expect(incrBtn).toBeInTheDocument()
-})
+        // we are verifying...
+        expect(countText).toBeInTheDocument()
+        expect(decrBtn).toBeInTheDocument()
+        expect(incrBtn).toBeInTheDocument()
+    })
+    test('incr by 1', () => {
+        render(<Counter></Counter>);
+        const incrBtn = screen.getByText('Increment');
+        fireEvent.click(incrBtn);
+        const countTextOne = screen.getByText('Count is 1');
+        expect(countTextOne).toBeInTheDocument()
+    })
+    test('decr by 2', () => {
+        render(<Counter></Counter>);
+        const decrBtn = screen.getByText('Decrement');
+        fireEvent.click(decrBtn);
+        fireEvent.click(decrBtn);
+        const countTextMinusTwo = screen.getByText('Count is -2');
+        expect(countTextMinusTwo).toBeInTheDocument()
+    })
 
-test('incr by 1', () => {
+    // snapshot...
+    test('snapshot for counter', () => {
+        const { asFragment } = render(<Counter />)
+        expect(asFragment()).toMatchSnapshot()
 
-})
-
-test('decr by 1', () => {
-
+    })
 })
